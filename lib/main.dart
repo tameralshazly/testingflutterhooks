8a0b0526aa14d24e34f55686136c7c8638015b86
tameralshazly <tameralshazly@yahoo.com>
2023-07-29 19:35:17 +0400

    step-02

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(
@@ -12,14 +13,22 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+Stream<String> getTime() => Stream.periodic(
+      const Duration(seconds: 1),
+      (_) => DateTime.now().toIso8601String(),
+    );
+
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dateTime = useStream(
+      getTime(),
+    );
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: Text(dateTime.data ?? 'Home Page'),
       ),
     );
   }
