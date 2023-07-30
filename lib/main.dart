8c84ce5c2684740a2b4d5e75a37e91fbb364049e
tameralshazly <tameralshazly@yahoo.com>
2023-07-30 14:54:39 +0400

    step-07

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -17,83 +17,37 @@ void main() {
 
 const url =
     'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=740&t=st=1690669120~exp=1690669720~hmac=5a1d0d3a7a31c9034efa1fbf4a11504e366be29357555ddd6e2cbaf8153846fa';
-const imageHeight = 300.0;
-
-extension Normalize on num {
-  num normalize(
-    num selfRangeMin,
-    num selfRangeMax, [
-    num normalizedRangeMin = 0.0,
-    num normalizedRangeMax = 1.0,
-  ]) =>
-      (normalizedRangeMax - normalizedRangeMin) *
-          ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) +
-      normalizedRangeMin;
-}
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final opacity = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1.0,
-      lowerBound: 0.0,
-      upperBound: 1.0,
-    );
-
-    final size = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1.0,
-      lowerBound: 0.0,
-      upperBound: 1.0,
-    );
-    final controller = useScrollController();
-    useEffect(() {
-      controller.addListener(() {
-        final newOpacity = max(imageHeight - controller.offset, 0.0);
-        final normalized = newOpacity.normalize(0.0, imageHeight).toDouble();
-        opacity.value = normalized;
-        size.value = normalized;
-      });
-      return null;
-    }, [controller]);
-
+    late final StreamController<double> controller;
+    controller = useStreamController<double>(onListen: () {
+      controller.sink.add(0.0);
+    });
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Column(
-        children: [
-          SizeTransition(
-            sizeFactor: size,
-            axis: Axis.vertical,
-            axisAlignment: -1.0,
-            child: FadeTransition(
-              opacity: opacity,
-              child: Image.network(
-                url,
-                height: imageHeight,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              controller: controller,
-              itemCount: 100,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(
-                    'Person ${index + 1}',
-                  ),
-                );
-              },
-            ),
-          )
-        ],
-      ),
+      body: StreamBuilder<double>(
+          stream: controller.stream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            } else {
+              final rotation = snapshot.data ?? 0.0;
+              return GestureDetector(
+                onTap: () {
+                  controller.sink.add(rotation + 10.0);
+                },
+                child: RotationTransition(
+                    turns: AlwaysStoppedAnimation(rotation / 360),
+                    child: Center(child: Image.network(url))),
+              );
+            }
+          }),
     );
   }
 }
